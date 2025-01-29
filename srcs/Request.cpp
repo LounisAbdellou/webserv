10eb6f65c048db9c73086e80133fc0b409f98a7a
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2025-01-29 14:44:28 +0100

    CU-86c1v1cym - Class Request

--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -1,24 +1,41 @@
 #include "Request.hpp"
+#include "AHttpMessage.hpp"
 
-Request::Request()
-{
+Request::Request() : AHttpMessage() {
+  this->path = "";
+  this->method = "";
+  this->contentType = "";
 }
 
-Request::Request(const Request& cpy)
-{
-  (void)cpy;
+Request::Request(const Request &src) : AHttpMessage(src) { *this = src; }
+
+Request &Request::operator=(const Request &src) {
+  if (this != &src) {
+    this->path = src.path;
+    this->method = src.method;
+    this->contentType = src.contentType;
+  }
+
+  return *this;
 }
 
-Request& Request::operator=(Request& cpy)
-{
-  return cpy;
+void Request::parseData() {
+  bool isPath = false;
+
+  for (size_t i = 0; i < this->_rawData.size(); i++) {
+    if (this->_rawData[i] == ' ' && !isPath) {
+      isPath = true;
+      continue;
+    } else if (this->_rawData[i] == ' ' && isPath) {
+      break;
+    }
+
+    if (!isPath) {
+      this->method.push_back(this->_rawData[i]);
+    } else {
+      this->path.push_back(this->_rawData[i]);
+    }
+  }
 }
 
-Request::~Request()
-{
-}
-
-void  Request::clean()
-{
-
-}
+void Request::clean() {}
